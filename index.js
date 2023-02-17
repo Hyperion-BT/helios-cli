eb67aaaf2c0dde17cadbb7e6373d269566b1cbb9
Christian Schmitz <cschmitz398@gmail.com>
2023-02-17 01:17:58 -0600

    lib bump

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import {
 	VERSION as HELIOS_VERSION
 } from 'helios';
 
-const VERSION = "0.12.0";
+const VERSION = "0.12.1";
 
 const USAGE = `Usage:
   helios [-h|--help] <command> <command-options>
